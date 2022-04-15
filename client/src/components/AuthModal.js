04a8d8764bbb9eaf06a633aab0bdaae016b69f91
Tdoval <thiagodovalaraujo@gmail.com>
2022-04-14 21:02:43 -0300

    Signing Users Up - MongoClient insertOne

--- client/src/components/AuthModal.js
+++ client/src/components/AuthModal.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const AuthModal = ({ setShowModal, isSignUp }) => {
   const [email, setEmail] = useState(null);
@@ -6,17 +8,30 @@ const AuthModal = ({ setShowModal, isSignUp }) => {
   const [confirmPassword, setConfirmPassword] = useState(null);
   const [error, setError] = useState(null);
 
+  let navigate = useNavigate();
+
   const handleClick = () => {
     setShowModal(false);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       if (isSignUp && password !== confirmPassword) {
         setError('Passwords need to match!');
+        return;
       }
-      console.log('make a post request to our database');
+
+      const response = await axios.post('http://localhost:8000/signup', {
+        email,
+        password,
+      });
+
+      const success = response.status === 201;
+
+      if (success) {
+        navigate('/onboarding');
+      }
     } catch (error) {
       console.log(error);
     }
